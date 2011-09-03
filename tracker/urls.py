4b1d4c8fd4418b328423836f35bf384b3c43ae07
Benjamin Cutler <cutler@cs.colostate.edu>
2011-09-03 03:12:47 -0600

    Added a bunch of new pages

--- tracker/urls.py
+++ tracker/urls.py
@@ -2,6 +2,14 @@ from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('tracker.views',
 	url(r'^$', 'index'),
-	url(r'^challenge/$', 'challengeindex'),
-	url(r'^choice/$', 'choiceindex'),
+	url(r'^challenges/$', 'challengeindex'),
+	url(r'^choices/$', 'choiceindex'),
+	url(r'^choice/(?P<id>-?\d+)/$', 'choice'),
+	url(r'^choiceoption/(?P<id>-?\d+)/$', 'choiceoption'),
+	url(r'^donors/$', 'donorindex'),
+	url(r'^donor/(?P<id>-?\d+)/$', 'donor'),
+	url(r'^donations/$', 'donationindex'),
+	url(r'^donation/(?P<id>-?\d+)/$', 'donation'),
+	url(r'^games/$', 'gameindex'),
+	url(r'^game/(?P<id>-?\d+)/$', 'game'),
 )
--- tracker/views.py
+++ tracker/views.py
@@ -1,6 +1,6 @@
 # Create your views here.
 from django.shortcuts import render_to_response
-from django.db.models import Sum
+from django.db.models import Count,Sum,Max,Avg
 from donations.tracker.models import *
 import django.shortcuts
 import locale
@@ -10,14 +10,55 @@ def redirect(request):
 
 def index(request):
 	locale.setlocale( locale.LC_ALL, '')
-	total = locale.currency(Donation.objects.aggregate(Sum('amount'))['amount__sum'], grouping=True)
-	return render_to_response('tracker/index.html', { 'total' : total})
+	agg = Donation.objects.filter(amount__gt="0.0").aggregate(Sum('amount'), Max('amount'), Avg('amount'))
+	count = Donation.objects.filter(amount__gt="0.0").count()
+	total,mx,av = locale.currency(agg['amount__sum'], grouping=True),locale.currency(agg['amount__max'], grouping=True),locale.currency(agg['amount__avg'], grouping=True)
+	return render_to_response('tracker/index.html', { 'count' : count, 'total' : total, 'mx' : mx, 'av' : av })
 	
 def challengeindex(request):
 	challenges = Challenge.objects.values('challengeId', 'name', 'goal', 'speedRun__name').order_by('speedRun__name').annotate(Sum('challengebid__amount'))
 	return render_to_response('tracker/challengeindex.html', { 'challenges' : challenges })
 	
 def choiceindex(request):
-	choices = Choice.objects.values('choiceId', 'name', 'speedRun__name', 'choiceoption__name').order_by('speedRun__name','name','choiceoption__name').annotate(Sum('choiceoption__choicebid__amount'))
+	choices = Choice.objects.values('choiceId', 'name', 'speedRun__speedRunId', 'speedRun__name', 'choiceoption__optionId', 'choiceoption__name').order_by('speedRun__name','name','choiceoption__name').annotate(Sum('choiceoption__choicebid__amount'))
 	return render_to_response('tracker/choiceindex.html', { 'choices' : choices })
+	
+def choice(request,id):
+	choice = Choice.objects.get(pk=id)
+	options = ChoiceOption.objects.filter(choice__exact=id).annotate(Sum('choicebid__amount'), Count('choicebid__amount'))
+	print choice
+	print options
+	return render_to_response('tracker/choice.html', { 'choice' : choice, 'options' : options })
 		
+def choiceoption(request,id):
+	choiceoption = ChoiceOption.objects.get(pk=id)
+	choicebids = ChoiceBid.objects.values('donationId', 'donationId__donorId', 'donationId__donorId__firstName','donationId__donorId__lastName', 'amount', 'donationId__timeReceived').filter(optionId__exact=id).order_by('donationId__timeReceived')
+	print choiceoption
+	print choicebids
+	return render_to_response('tracker/choiceoption.html', { 'choiceoption' : choiceoption, 'choicebids' : choicebids })
+	
+def donorindex(request):
+	locale.setlocale( locale.LC_ALL, '')
+	donors = Donor.objects.all().filter(lastName__isnull=False).order_by('lastName', 'firstName').annotate(Sum('donation__amount'), Count('donation__amount'), Max('donation__amount'), Avg('donation__amount'))
+	return render_to_response('tracker/donorindex.html', { 'donors' : donors })
+	
+def donor(request,id):
+	locale.setlocale( locale.LC_ALL, '')
+	donor = Donor.objects.get(pk=id)
+	donations = Donation.objects.filter(donorId__exact=id)
+	agg = donations.aggregate(Sum('amount'), Count('amount'), Max('amount'), Avg('amount'))
+	total,mx,av = locale.currency(agg['amount__sum'], grouping=True),locale.currency(agg['amount__max'], grouping=True),locale.currency(agg['amount__avg'], grouping=True)
+	return render_to_response('tracker/donor.html', { 'donor' : donor, 'donations' : donations, 'agg' : agg, 'total' : total, 'mx' : mx, 'av' : av })
+	
+def donationindex(request):
+	return index(request)
+
+def donation(request,id):
+	donation = Donation.objects.get(pk=id)
+	return render_to_response('tracker/donation.html', { 'donation' : donation })
+	
+def gameindex(request):
+	return index(request)
+
+def game(request,id):
+	return index(request)
