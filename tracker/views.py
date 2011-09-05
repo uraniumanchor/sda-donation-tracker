dfa63e04f424a3814a07eeae297a9f29da73c92a
Benjamin Cutler <cutler@cs.colostate.edu>
2011-09-05 14:34:34 -0600

    Added more sorting and information to various pages

--- tracker/views.py
+++ tracker/views.py
@@ -1,5 +1,5 @@
 # Create your views here.
-from django.shortcuts import render_to_response
+from django.shortcuts import render,render_to_response
 from django.db.models import Count,Sum,Max,Avg
 from django.db.utils import ConnectionDoesNotExist
 from django.core.exceptions import ObjectDoesNotExist
@@ -35,7 +35,7 @@ def fixorder(queryset, orderdict, sort, order):
 	if len(orderdict[sort]) == 2:
 		queryset = queryset.order_by(orderdict[sort][0], orderdict[sort][1])
 	else:
-		queryset = queryset.order_by(orderdict[sort])
+		queryset = queryset.order_by(orderdict[sort][0])
 	if order == -1:
 		queryset = queryset.reverse()
 	return queryset
@@ -61,14 +61,16 @@ def logout(request):
 	auth_logout(request)
 	return django.shortcuts.redirect(request.META.get('HTTP_REFERER', '/'))	
 	
-def tracker_response(request, db, template, dict={}):
-	dict.update({ 'dbtitle' : settings.DATABASES[checkdb(db)]['COMMENT'], 'usernames' : request.user.has_perm('tracker.view_usernames'), 'emails' : request.user.has_perm("tracker.view_emails"), 'djangoversion' : dv(), 'pythonversion' : pv(), 'db' : db, 'user' : request.user, 'form' : AuthenticationForm(request), 'next' : request.path })
-	return render_to_response(template, dict, context_instance=RequestContext(request))
+def tracker_response(request, db=None, template='tracker/index.html', dict={}, status=200):
+	if db:
+		dict.update({ 'db' : db, 'dbtitle' : settings.DATABASES[checkdb(db)]['COMMENT'] })
+	dict.update({ 'usernames' : request.user.has_perm('tracker.view_usernames'), 'emails' : request.user.has_perm("tracker.view_emails"), 'djangoversion' : dv(), 'pythonversion' : pv(), 'user' : request.user, 'form' : AuthenticationForm(request), 'next' : request.path })
+	return render(request, template, dictionary=dict, status=status)
 	
 def dbindex(request):
 	dbs = settings.DATABASES.copy()
 	del dbs['default']
-	return trackerresponse(request, None, 'tracker/dbindex.html', { 'databases' : dbs })
+	return tracker_response(request, None, 'tracker/dbindex.html', { 'databases' : dbs })
 
 def index(request,db=''):
 	try:
@@ -79,7 +81,7 @@ def index(request,db=''):
 		total,mx,av = locale.currency(agg['amount__sum'], grouping=True),locale.currency(agg['amount__max'], grouping=True),locale.currency(agg['amount__avg'], grouping=True)
 		return tracker_response(request, db, 'tracker/index.html', { 'count' : count, 'total' : total, 'mx' : mx, 'av' : av })
 	except ConnectionDoesNotExist:
-		return render_to_response('tracker/baddatabase.html')
+		return tracker_response(request, template='tracker/baddatabase.html', status=404)
 	
 def challengeindex(request,db):
 	try:
@@ -87,7 +89,7 @@ def challengeindex(request,db):
 		challenges = Challenge.objects.using(database).values('challengeId', 'name', 'goal', 'speedRun', 'speedRun__name').order_by('speedRun__name').annotate(Sum('challengebid__amount'))
 		return tracker_response(request, db, 'tracker/challengeindex.html', { 'challenges' : challenges })
 	except ConnectionDoesNotExist:
-		return render_to_response('tracker/baddatabase.html')
+		return tracker_response(request, template='tracker/baddatabase.html', status=404)
 	
 def challenge(request,id,db):
 	try:
@@ -96,9 +98,9 @@ def challenge(request,id,db):
 		bids = ChallengeBid.objects.filter(challenge__exact=id).values('amount', 'donation', 'donation__donorId', 'donation__timeReceived', 'donation__donorId__firstName', 'donation__donorId__lastName', 'donation__donorId__email')
 		return tracker_response(request, db, 'tracker/challenge.html', { 'challenge' : challenge, 'bids' : bids })
 	except ObjectDoesNotExist:
-		return render_to_response('tracker/badobject.html')
+		return tracker_response(request, db, template='tracker/badobject.html', status=404)
 	except ConnectionDoesNotExist:
-		return render_to_response('tracker/baddatabase.html')
+		return tracker_response(request, template='tracker/baddatabase.html', status=404)
 	
 def choiceindex(request,db):
 	try:
@@ -106,7 +108,7 @@ def choiceindex(request,db):
 		choices = Choice.objects.using(database).values('choiceId', 'name', 'speedRun', 'speedRun__name', 'choiceoption', 'choiceoption__name').annotate(Sum('choiceoption__choicebid__amount')).order_by('speedRun__name','name','-choiceoption__choicebid__amount__sum')
 		return tracker_response(request, db, 'tracker/choiceindex.html', { 'choices' : choices })
 	except ConnectionDoesNotExist:
-		return render_to_response('tracker/baddatabase.html')
+		return tracker_response(request, template='tracker/baddatabase.html', status=404)
 	
 def choice(request,id,db='default'):
 	try:
@@ -115,28 +117,38 @@ def choice(request,id,db='default'):
 		options = ChoiceOption.objects.using(database).filter(choice__exact=id).annotate(Sum('choicebid__amount'), Count('choicebid__amount'))
 		return tracker_response(request, db, 'tracker/choice.html', { 'choice' : choice, 'options' : options })
 	except ObjectDoesNotExist:
-		return render_to_response('tracker/badobject.html')
+		return tracker_response(request, db, template='tracker/badobject.html', status=404)
 	except ConnectionDoesNotExist:
-		return render_to_response('tracker/baddatabase.html')
+		return tracker_response(request, template='tracker/baddatabase.html', status=404)
 		
 def choiceoption(request,id,db='default'):
 	try:
+		orderdict = { 
+			'name'   : ('donationId__donorId__lastName', 'donationId__donorId__firstName'),
+			'amount' : ('amount', ),
+			'time'   : ('donationId__timeReceived', ),
+		}
+		sort = request.GET.get('sort', 'time')
+		if sort not in orderdict:
+			sort = 'time'
+		order = int(request.GET.get('order', '-1'))
 		database = checkdb(db)
 		choiceoption = ChoiceOption.objects.using(database).get(pk=id)
-		choicebids = ChoiceBid.objects.using(database).values('donationId', 'donationId__donorId', 'donationId__donorId__firstName','donationId__donorId__lastName', 'donationId__donorId__email', 'amount', 'donationId__timeReceived').filter(optionId__exact=id).order_by('donationId__timeReceived')
+		choicebids = ChoiceBid.objects.using(database).values('donationId', 'donationId__donorId', 'donationId__donorId__firstName','donationId__donorId__lastName', 'donationId__donorId__email', 'amount', 'donationId__timeReceived').filter(optionId__exact=id)
+		choicebids = fixorder(choicebids, orderdict, sort, order)
 		return tracker_response(request, db, 'tracker/choiceoption.html', { 'choiceoption' : choiceoption, 'choicebids' : choicebids })
 	except ObjectDoesNotExist:
-		return render_to_response('tracker/badobject.html')
+		return tracker_response(request, db, template='tracker/badobject.html', status=404)
 	except ConnectionDoesNotExist:
-		return render_to_response('tracker/baddatabase.html')
+		return tracker_response(request, template='tracker/baddatabase.html', status=404)
 
 def donorindex(request,db='default'):
 	try:
 		orderdict = { 
-			'name' : ('lastName', 'firstName'),
-			'total' : ('donation__amount__sum'),
-			'max' : ('donation__amount__max'),
-			'avg' : ('donation__amount__avg')
+			'name'  : ('lastName',              'firstName'),
+			'total' : ('donation__amount__sum',            ),
+			'max'   : ('donation__amount__max',            ),
+			'avg'   : ('donation__amount__avg',            )
 		}
 		sort = request.GET.get('sort', 'name')
 		if sort not in orderdict:
@@ -148,7 +160,7 @@ def donorindex(request,db='default'):
 		donors = fixorder(donors, orderdict, sort, order)
 		return tracker_response(request, db, 'tracker/donorindex.html', { 'donors' : donors })
 	except ConnectionDoesNotExist:
-		return render_to_response('tracker/baddatabase.html')
+		return tracker_response(request, template='tracker/baddatabase.html', status=404)
 	
 def donor(request,id,db='default'):
 	try:
@@ -160,16 +172,16 @@ def donor(request,id,db='default'):
 		total,mx,av = locale.currency(agg['amount__sum'], grouping=True),locale.currency(agg['amount__max'], grouping=True),locale.currency(agg['amount__avg'], grouping=True)
 		return tracker_response(request, db, 'tracker/donor.html', { 'donor' : donor, 'donations' : donations, 'agg' : agg, 'total' : total, 'mx' : mx, 'av' : av })
 	except ObjectDoesNotExist:
-		return render_to_response('tracker/badobject.html')
+		return tracker_response(request, db, template='tracker/badobject.html', status=404)
 	except ConnectionDoesNotExist:
-		return render_to_response('tracker/baddatabase.html')
+		return tracker_response(request, template='tracker/baddatabase.html', status=404)
 	
 def donationindex(request,db='default'):
 	try:
 		orderdict = { 
-			'name' : ('donorId__lastName', 'donorId__firstName'),
-			'amount' : ('amount'),
-			'time' : ('timeReceived'),
+			'name'   : ('donorId__lastName', 'donorId__firstName'),
+			'amount' : ('amount', ),
+			'time'   : ('timeReceived', ),
 		}
 		sort = request.GET.get('sort', 'time')
 		if sort not in orderdict:
@@ -180,18 +192,20 @@ def donationindex(request,db='default'):
 		donations = fixorder(donations, orderdict, sort, order)
 		return tracker_response(request, db, 'tracker/donationindex.html', { 'donations' : donations })
 	except ConnectionDoesNotExist:
-		return render_to_response('tracker/baddatabase.html')
+		return tracker_response(request, template='tracker/baddatabase.html', status=404)
 
 def donation(request,id,db='default'):
 	try:
 		database = checkdb(db)
 		donation = Donation.objects.using(database).get(pk=id)
 		donor = donation.donorId
-		return tracker_response(request, db, 'tracker/donation.html', { 'donation' : donation, 'donor' : donor })
+		choicebids = ChoiceBid.objects.using(database).filter(donationId__exact=id).values('amount', 'optionId', 'optionId__name', 'optionId__choice', 'optionId__choice__name', 'optionId__choice__speedRun', 'optionId__choice__speedRun__name')
+		challengebids = ChallengeBid.objects.using(database).filter(donation__exact=id).values('amount', 'challenge', 'challenge__name', 'challenge__goal', 'challenge__speedRun', 'challenge__speedRun__name')
+		return tracker_response(request, db, 'tracker/donation.html', { 'donation' : donation, 'donor' : donor, 'choicebids' : choicebids, 'challengebids' : challengebids })
 	except ObjectDoesNotExist:
-		return render_to_response('tracker/badobject.html')
+		return tracker_response(request, db, template='tracker/badobject.html', status=404)
 	except ConnectionDoesNotExist:
-		return render_to_response('tracker/baddatabase.html')
+		return tracker_response(request, template='tracker/baddatabase.html', status=404)
 
 def gameindex(request,db='default'):
 	try:
@@ -199,7 +213,7 @@ def gameindex(request,db='default'):
 		games = SpeedRun.objects.using(database).all()
 		return tracker_response(request, db, 'tracker/gameindex.html', { 'games' : games })
 	except ConnectionDoesNotExist:
-		return render_to_response('tracker/baddatabase.html')
+		return tracker_response(request, template='tracker/baddatabase.html', status=404)
 
 def game(request,id,db='default'):
 	try:
@@ -209,9 +223,9 @@ def game(request,id,db='default'):
 		choices = Choice.objects.using(database).filter(speedRun__exact=game).values('choiceId', 'name', 'choiceoption', 'choiceoption__name',).annotate(Sum('choiceoption__choicebid__amount')).order_by('name', '-choiceoption__choicebid__amount__sum')
 		return tracker_response(request, db, 'tracker/game.html', { 'game' : game, 'challenges' : challenges, 'choices' : choices })
 	except ObjectDoesNotExist:
-		return render_to_response('tracker/badobject.html')
+		return tracker_response(request, db, template='tracker/badobject.html', status=404)
 	except ConnectionDoesNotExist:
-		return render_to_response('tracker/baddatabase.html')
+		return tracker_response(request, template='tracker/baddatabase.html', status=404)
 	
 def prizeindex(request,db='default'):
 	try:
@@ -219,4 +233,4 @@ def prizeindex(request,db='default'):
 		prizes = Prize.objects.using(database).values('name', 'description', 'image', 'donor', 'donor__firstName', 'donor__lastName', 'donor__email')
 		return tracker_response(request, db, 'tracker/prizeindex.html', { 'prizes' : prizes })
 	except ConnectionDoesNotExist:
-		return render_to_response('tracker/baddatabase.html')
+		return tracker_response(request, template='tracker/baddatabase.html', status=404)
