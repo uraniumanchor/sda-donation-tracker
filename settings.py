d0cf1062463c33b27531597156e0af7f8cc12b78
Benjamin Cutler <cutler@cs.colostate.edu>
2011-09-07 23:17:51 -0600

    Added "real" locale support

--- settings.py
+++ settings.py
@@ -98,11 +98,12 @@ TEMPLATE_LOADERS = (
 )
 
 MIDDLEWARE_CLASSES = (
-    'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.common.CommonMiddleware',
 )
 
 ROOT_URLCONF = 'donations.urls'
