6f3695bcad5e33ac362da6ba37f71dbfc6ba502f
Benjamin Cutler <cutler@cs.colostate.edu>
2011-09-08 00:26:25 -0600

    Made some of the directories more dynamic

--- settings.py
+++ settings.py
@@ -50,7 +50,7 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = '/home/uraniumanchor/donations.uranium-anchor.com/media/'
+MEDIA_ROOT = os.getenv('HOME') + '/donations.uranium-anchor.com/media/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -61,7 +61,7 @@ MEDIA_URL = 'http://donations.uranium-anchor.com/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/home/uranium-anchor/django-static/'
+STATIC_ROOT = os.getenv('HOME') + '/django-static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -112,8 +112,8 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/home/uraniumanchor/django-templates',
-    '/home/ioti/django/templates',
+    os.getenv('HOME') + '/django-templates',
+    os.getenv('HOME') + '/django/templates',
     '/home/coolmatty/templates'
 )
 
